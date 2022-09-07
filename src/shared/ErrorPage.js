a3811f5dbece3e789654561874074379f386577c
Deepakkumar-kaaylabs <108456757+Deepakkumar-kaaylabs@users.noreply.github.com>
2022-09-07 16:10:24 +0530

    side bar disabled for 404

--- src/shared/ErrorPage.js
+++ src/shared/ErrorPage.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import "./sidebar.css"
 import {useNavigate} from 'react-router-dom'
+import img from "./404.png"
 const ErrorPage = () => {
   const navigate=useNavigate()
   return (
     <div className='errorPage'>
-          <img src='https://miro.medium.com/max/875/1*DeBkx8vjbumpCO-ZkPE9Cw.png' />
+          <img src={img} />
 
               <div className='homeBtn' onClick={()=>navigate('/')}>Back To Home</div>
         </div >
