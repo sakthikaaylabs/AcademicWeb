54e5b701ea152baedfafa3774f24218695869516
sakthikaaylabs <sakthivel.arumugapandian@kaaylabs.com>
2022-09-07 12:46:56 +0530

    structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
             <h1>
                 AcadmicMeat
             </h1>
-        </p>
-        <p>
-          start Sep1
         </p>
       </header>
     </div>
