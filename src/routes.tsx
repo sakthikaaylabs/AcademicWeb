2c1baa27eb7381822ccbaeb11fc1027bbb7ddb18
Manoradha77 <manokaran.radhakrishnan@kaaylabs.com>
2022-09-12 11:47:58 +0530

    login-signup UI implementation

--- src/routes.tsx
+++ src/routes.tsx
@@ -6,6 +6,9 @@ import Academy from './academy/academy';
 import Create from './create/create';
 import Group from './groups/group';
 import Profile from './Profile/profile';
+import Login from './Login/login';
+import Signup from './createAccount/createAccount';
+import ChangePassword from "./changePassword/changePassword";
 import Report from './reports/report';
 // import Notfound from './shared/404notfount/notfound'
 import Sidebar from './shared/Sidebar/sideBar'
@@ -14,7 +17,7 @@ const Routers = () => {
     return (
         <div>
             <BrowserRouter>
-                <Sidebar/>
+                {/* <Sidebar/> */}
                 <div style={{ textAlign: "center" }}>
                     <Routes>
                         <Route path='/' element={<Dashboard />} />
@@ -24,6 +27,9 @@ const Routers = () => {
                         <Route path="/report" element={<Report />} />
                         <Route path="/profile" element={<Profile />} />
                         <Route path="/group" element={<Group />} />
+                        <Route path="/login" element={<Login />} />
+                        <Route path="/signup" element={<Signup />} />
+                        <Route path="/changePassword" element={<ChangePassword />} />
                     </Routes>
                 </div>
             </BrowserRouter>
